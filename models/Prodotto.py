aeee3357ad680e112f9a4d74b27c598443f8e7d0
PietroLagioia <133354833+PietroLagioia@users.noreply.github.com>
2024-01-02 13:14:25 +0100

    Create Prodotto.py

---
+++ models/Prodotto.py
@@ -0,0 +1,64 @@
+import mysql.connector
+from utils import mysql_config
+
+conn = mysql_config.get_database_connection()
+cursor = conn.cursor()
+
+def update_prodotto(product_id, **kwargs):
+    update_query = "UPDATE prodotto SET "
+
+    update_data = []
+    for key, value in kwargs.items():
+        update_query += f"{key}=%s, "
+        update_data.append(value)
+
+    update_query = update_query.rstrip(', ')
+    update_query += " WHERE id_prodotto=%s"
+    update_data.append(product_id)
+
+    try:
+        cursor.execute(update_query, update_data)
+
+        conn.commit()
+        print(f"Prodotto con ID {product_id} aggiornato correttamente.")
+    except mysql.connector.Error as err:
+        print(f"Errore durante l'aggiornamento del prodotto")
+
+
+def delete(product_id):
+    delete_query = "DELETE FROM prodotto WHERE id_prodotto=%s"
+
+    try:
+        cursor.execute(delete_query, (product_id,))
+
+        conn.commit()
+        print(f"Prodotto con ID {product_id} eliminato correttamente.")
+    except mysql.connector.Error as err:
+        print(f"Errore durante l'eliminazione del prodotto")
+
+class Prodotto:
+    def __init__(self, nome, categoria, marca, descrizione, vestibilita, prezzo, colore, materiale):
+        self.nome = nome
+        self.categoria = categoria
+        self.marca = marca
+        self.descrizione = descrizione
+        self.vestibilita = vestibilita
+        self.prezzo = prezzo
+        self.colore = colore
+        self.materiale = materiale
+
+    def save(self):
+        insert_query = ("INSERT INTO prodotto "
+                        "(nome, categoria, marca, descrizione, vestibilità, prezzo, colore, materiale) "
+                        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+
+        product_data = (self.nome, self.categoria, self.marca, self.descrizione,
+                        self.vestibilita, self.prezzo, self.colore, self.materiale)
+
+        try:
+            cursor.execute(insert_query, product_data)
+
+            conn.commit()
+            print("Prodotto salvato correttamente nel database.")
+        except mysql.connector.Error as err:
+            print(f"Errore durante l'inserimento del prodotto")
