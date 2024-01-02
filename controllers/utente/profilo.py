6e461a7463ac1361ee8c8d49f39f38202e630057
Alfredo Baratta <63536128+alfredo-baratta@users.noreply.github.com>
2024-01-02 10:24:44 +0100

    Update profilo.py

--- controllers/utente/profilo.py
+++ controllers/utente/profilo.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 app_bp = Blueprint('user_profile', __name__)
 
 
-@app_bp.route("/profilo")
+@app_bp.route("/utente/profilo")
 def profilo():
     return render_template("utente/profilo.html")
