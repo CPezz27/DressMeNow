90496e4caf27ae72a82b2624a6cbe95bcbd2ce8e
Alfredo Baratta <63536128+alfredo-baratta@users.noreply.github.com>
2024-01-03 17:42:40 +0100

    Update Prodotto.py

--- models/Prodotto.py
+++ models/Prodotto.py
@@ -51,24 +51,14 @@ def view_product(product_id):
         size_result = cursor.fetchall()
 
         if result and image_result:
-            result_images = []
-            for image in image_result:
-                result_images.append({
-                    'id_immagine': image[0],
-                    'id_prodotto': image[1],
-                    'immagine': image[2],
-                    'tipo': image[3]
-                })
-
-            result_sizes = []
-            for size in size_result:
-                result_sizes.append({
-                    'nome_taglia': size[0],
-                    'qta': size[1]
-                })
-
-            result[9] = result_images
-            result[10] = result_sizes
+            result[9] = [
+                {'id_immagine': img[0], 'id_prodotto': img[1], 'immagine': img[2], 'tipo': img[3]}
+                for img in image_result
+            ]
+            result[10] = [
+                {'nome_taglia': size[0], 'qta': size[1]}
+                for size in size_result
+            ]
             return result
         else:
             return None
