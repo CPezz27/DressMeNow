12f886c5a4f5f91519e3baba1a4d070459f8af51
Alfredo Napoli <a.napoli109@studenti.unisa.it>
2024-01-07 11:37:58 +0100

    modifichina stupidina ma importante
    E' importante perchè altrimenti il login non funziona

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from controllers import index
 from controllers.utente import login, profilo, registrati, effettua_ordine, carrello, prodotti
-from controllers.personale import login
+from controllers.personale import login as login_personale
 from controllers.personale.direttore import direttore
 from controllers.personale.gestore_prodotti import prodotti as prodotti_gestore
 from controllers.personale.gestore_ordini import ordini
@@ -12,6 +12,7 @@ app.config['SECRET_KEY'] = 'secret_key'
 
 app.register_blueprint(index.app_bp)
 app.register_blueprint(login.app_bp)
+app.register_blueprint(login_personale.app_bp)
 app.register_blueprint(profilo.app_bp)
 app.register_blueprint(registrati.app_bp)
 app.register_blueprint(effettua_ordine.app_bp)
