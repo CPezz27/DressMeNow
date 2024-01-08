2c50d93eb649bdb257df07e024321c5b8fc5718f
Alfredo Napoli <a.napoli109@studenti.unisa.it>
2024-01-08 19:05:47 +0100

    update homepage

--- controllers/index.py
+++ controllers/index.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+from models import Prodotto
 
 app_bp = Blueprint('index', __name__)
 
 @app_bp.route('/')
 def homepage():
-     return render_template('index.html')
+     results_uomo = Prodotto.view_products_by_category("uomo")
+     results_donna = Prodotto.view_products_by_category("donna")
+     results_bambino = Prodotto.view_products_by_category("bambino")
+     return render_template('index.html', res_u = results_uomo, res_d = results_donna, res_b = results_bambino)
