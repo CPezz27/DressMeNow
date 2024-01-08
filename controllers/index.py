cf30d6085dc3e65059f0608f0e0926b004332724
Alfredo Napoli <a.napoli109@studenti.unisa.it>
2024-01-08 19:42:50 +0100

    Homepage migliorata
    Ora vengono presi solo 3 prodotti dal DB per categoria e vengono mostrati nella homepage

--- controllers/index.py
+++ controllers/index.py
@@ -5,13 +5,13 @@ app_bp = Blueprint('index', __name__)
 
 @app_bp.route('/')
 def homepage():
-     results_uomo = Prodotto.view_products_by_category("uomo")
+     results_uomo = Prodotto.view_products_by_category("uomo")[:3]
      print("PRODOTTI UOMO:")
      print(results_uomo)
-     results_donna = Prodotto.view_products_by_category("donna")
+     results_donna = Prodotto.view_products_by_category("donna")[:3]
      print("PRODOTTI DONNA:")
      print(results_donna)
-     results_bambino = Prodotto.view_products_by_category("bambino")
+     results_bambino = Prodotto.view_products_by_category("bambino")[:3]
      print("PRODOTTI bambino:")
      print(results_bambino)
      return render_template('index.html', res_u = results_uomo, res_d = results_donna, res_b = results_bambino)
