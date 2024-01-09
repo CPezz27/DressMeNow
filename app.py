e18223c4d9ce377adfb5c1c6340937ca9d6f00bf
Alfredo Baratta <63536128+alfredo-baratta@users.noreply.github.com>
2024-01-09 16:07:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from controllers import index
-from controllers.utente import login, profilo, registrati, effettua_ordine, carrello, prodotti
+from controllers.utente import login, profilo, registrati, effettua_ordine, carrello, prodotti, indirizzi
 from controllers.personale import login as login_personale
 from controllers.personale.direttore import direttore
 from controllers.personale.gestore_prodotti import prodotti as prodotti_gestore, taglia
@@ -23,6 +23,7 @@ app.register_blueprint(prodotti_gestore.app_bp)
 app.register_blueprint(direttore.app_bp)
 app.register_blueprint(ordini.app_bp)
 app.register_blueprint(taglia.app_bp)
+app.register_blueprint(indirizzi.app_bp)
 
 
 if __name__ == '__main__':
