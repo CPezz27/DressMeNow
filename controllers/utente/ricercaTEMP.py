303b32b1f6f32d9eeb56f9b7cb4957c86263985c
Christian iervasi <c.iervasi@studenti.unisa.it>
2024-01-19 19:03:36 +0100

    update
    roba da fare ancora

--- controllers/utente/ricercaTEMP.py
+++ controllers/utente/ricercaTEMP.py
@@ -6,3 +6,8 @@ app_bp = Blueprint('user_NLP', __name__)
 @app_bp.route('/ricercaNLP', methods=['GET', 'POST'])
 def search():
     return render_template('/utente/ricerca_NLP.html')
+
+
+@app_bp.route('/risultatiNLP', methods=['GET', 'POST'])
+def search2():
+    return render_template('/utente/risultati_NLP.html')
