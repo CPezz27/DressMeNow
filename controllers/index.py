83cdb35f4d95b27f3660d528ecd3f5290f6b0563
Alfredo Baratta <63536128+alfredo-baratta@users.noreply.github.com>
2024-01-19 22:16:55 +0100

    Update index.py

--- controllers/index.py
+++ controllers/index.py
@@ -4,6 +4,21 @@ from models import Prodotto
 app_bp = Blueprint('index', __name__)
 
 
+@app_bp.errorhandler(403)
+def page_not_found(error):
+    return render_template('403.html'), 403
+
+
+@app_bp.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app_bp.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
+
+
 @app_bp.route('/')
 def homepage():
     results_uomo = Prodotto.view_products_by_category("uomo")[:3]
