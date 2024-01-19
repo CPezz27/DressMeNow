16d398600a6c5f2003bffa61a3d1d3a12f15e925
Alfredo Baratta <63536128+alfredo-baratta@users.noreply.github.com>
2024-01-19 22:13:52 +0100

    Update login.py

--- controllers/personale/login.py
+++ controllers/personale/login.py
@@ -3,13 +3,8 @@ from flask import Blueprint, render_template, request, redirect, session, url_fo
 
 from models import Personale
 from models import Prodotto
-from utils import mysql_config
 
 app_bp = Blueprint('direttore_login', __name__)
-
-conn = mysql_config.get_database_connection()
-
-cursor = conn.cursor()
 
 
 @app_bp.route('/direttore/login')
@@ -23,32 +18,28 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        if conn:
-            try:
-                direttore = Personale.login(email, password)
+        try:
+            direttore = Personale.login(email, password)
 
-                if direttore:
-                    session['logged_in'] = True
-                    session['email'] = email
+            if direttore:
+                session['logged_in'] = True
+                session['email'] = email
 
-                    tipo_personale = direttore[3]
+                tipo_personale = direttore[3]
 
-                    if tipo_personale == 'direttore':
-                        session['ruolo'] = 'direttore'
-                        return redirect(url_for('direttore_controller.visualizza_personale'))
-                    elif tipo_personale == 'gestore_ordine':
-                        session['ruolo'] = 'gestore_ordine'
-                        return redirect(url_for('order_controller.dashboard'))
-                    elif tipo_personale == 'gestore_prodotto':
-                        session['ruolo'] = 'gestore_prodotto'
-                        return redirect(url_for('direttore_login.gestore_prodotto'))
-                else:
-                    return "Credenziali non valide. Riprova."
-            except mysql.connector.Error as err:
-                print(f"Errore durante l'esecuzione della query")
-            finally:
-                cursor.close()
-                conn.close()
+                if tipo_personale == 'direttore':
+                    session['ruolo'] = 'direttore'
+                    return redirect(url_for('direttore_controller.visualizza_personale'))
+                elif tipo_personale == 'gestore_ordine':
+                    session['ruolo'] = 'gestore_ordine'
+                    return redirect(url_for('order_controller.dashboard'))
+                elif tipo_personale == 'gestore_prodotto':
+                    session['ruolo'] = 'gestore_prodotto'
+                    return redirect(url_for('direttore_login.gestore_prodotto'))
+            else:
+                return "Credenziali non valide. Riprova."
+        except Exception as err:
+            print(f"Errore durante l'esecuzione della query")
 
 
 @app_bp.route('/gestore_ordine')
@@ -80,11 +71,10 @@ def gestore_prodotto():
                     'materiale': prodotto[8],
                     'taglie': []  # Lista vuota per le taglie
                 }
-                
+
             # Recupera le taglie associate al prodotto
             taglie_prodotto = Prodotto.get_sizes_for_product(prodotto_id)  # Sostituisci con il metodo corretto
             for taglia in taglie_prodotto:
-
                 prodotti_dict[prodotto_id]['taglie'].append({
                     'id_taglia': taglia[0],
                     'nometaglia': taglia[1],
@@ -94,4 +84,3 @@ def gestore_prodotto():
         return render_template('gestoreProdotto.html', prodotti_da_mostrare=prodotti_finali)
     except Exception as err:
         return render_template('/direttore/login.html', messaggio="Errore durante la visualizzazione")
-
